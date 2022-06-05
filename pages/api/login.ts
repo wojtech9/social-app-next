0dd3f1535b4884863d5703e90c62f395351c5375
Wojtek <wojtek.tech9@gmail.com>
2022-06-05 19:53:51 +0200

    Login API

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -6,7 +6,44 @@ import jwt from 'jsonwebtoken';
 import user from '../../interfaces/User.interface';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// db imports
+
+import dbConnection from '../../utils/dbConnect';
+import { Users } from '../../models/Users';
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  try {
+    const userData = req.body;
+
+    if (!dbConnection.isInitialized) {
+      await dbConnection.initialize();
+    }
+    const UsersRepository = dbConnection.getRepository(Users);
+    const checkData: user | null = await UsersRepository.findOneBy({
+      nickname: userData.nickname,
+    });
+
+    // checking password if user exists
+    if (checkData) {
+      const result = await bcrypt.compare(
+        userData.password,
+        checkData.password
+      );
+      // invalid password
+      if (!result) {
+        throw new Error('invalid password');
+      }
+      // correct password
+      return res.status(200).json({ status: true });
+    }
+    // user doesn't exist
+    else {
+      throw new Error("User doesnt' exists");
+    }
+  } catch (e) {
+    return res.status(500).json({ status: false });
+  }
+}
