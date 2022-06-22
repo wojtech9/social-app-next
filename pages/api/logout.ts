1db86e832ea43d21141277587a95fc8cf88103af
Wojtek <wojtek.tech9@gmail.com>
2022-06-22 23:50:01 +0200

    Readme update

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res:NextApiResponse) { 
-
-}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {}
